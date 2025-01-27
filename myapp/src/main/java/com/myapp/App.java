efaa46a595d7a17e7a8b2fd7c530cd15002b2afb
TalSinay <94438376+TalSinay@users.noreply.github.com>
2025-01-27 20:02:02 +0200

    update Dockerfile path in dev.yml

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -5,6 +5,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! this is Tal Sinay 27.1." );
+        System.out.println( "Hello World! this is Tal Sinay." );
     }
 }
