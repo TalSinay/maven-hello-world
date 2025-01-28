5022f9992016aec3503dd32b8e7fc3bc018956d5
TalSinay <94438376+TalSinay@users.noreply.github.com>
2025-01-28 10:08:37 +0200

    update version

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -5,6 +5,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! this is Tal Sinay." );
+        System.out.println( "Hello World! This Is Tal Sinay." );
     }
 }
